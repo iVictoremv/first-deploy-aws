e573af1550ae57e8df71378f7d4acc1a0bba1794
Victor Mendoza <72841783+Viictoremv@users.noreply.github.com>
2024-02-29 15:52:09 -0600

    add deploy file

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+ngrok service stop
+sudo systemctl stop nginx
+cd /etc/nginx/
+sudo git clone "https://github.com/Viictoremv/first-deploy-aws.git"
+sudo systemctl start nginx
+sudo systemctl restart nginx
+sudo ngrok config add-authtoken 2NwleJ82ePjmg0q1ay7k3LSg5LN_3rRxEf1Adphju3q1Bhf9g
+sudo ngrok http 81
