6a5a8d10200a333fa7b6ef9317e4f9f903502839
Victor Mendoza <vmendoza2002@hotmail.com>
2024-03-06 19:54:00 -0600

    update deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 ngrok service stop
 sudo systemctl stop nginx
-cd /etc/nginx/first-deploy-aws
-sudo git pull origin main
+cd /etc/nginx/first-deploy-aws/
+sudo git pull
 sudo systemctl start nginx
 sudo systemctl restart nginx
 sudo ngrok config add-authtoken 2NwleJ82ePjmg0q1ay7k3LSg5LN_3rRxEf1Adphju3q1Bhf9g
